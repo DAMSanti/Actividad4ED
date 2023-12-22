b6c6b1f7b563c0f3a0b6e5c8d6bb225a82a45ecd
DAM109-ADRIAN <adrian.dediegofernandez@iesmiguelherrero.com>
2023-12-22 15:18:24 +0100

    realizo el cambio en la misma linea

---
+++ AdriySantiED/src/main/java/actividaded/adriysantied/AdriySantiED.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package actividaded.adriysantied;
+
+/**
+ *
+ * @author DAM103
+ */
+public class AdriySantiED {
+
+    public static void main(String[] args) {
+        System.out.println("Esto esta aprobado");
+    }
+}
